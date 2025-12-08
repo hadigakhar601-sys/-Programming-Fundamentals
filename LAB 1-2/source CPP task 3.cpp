454b7dcdacfb92015734649d7f5c09dd97df50e9
Abdul Hadi <hadigakhar601@gmail.com>
2025-12-08 12:58:50 +0500

    Add files via upload

---
+++ LAB 1-2/source CPP task 3.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main(){
+	cout << "Item\t\tPrice\t\tQuality\nPen\t\t$1\t\t10\nPencil\t\t$2\t\t5\nEraser\t\t$0.5\t\t20\n" ;
+	return 0 ;
+}
---
+++ LAB 1-2/source CPP task 4.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std ;
+int main(){
+	cout << "She said, \"Let's Learn C++\"\n" ;
+	cout << "To start, save your file in C:C++_projects\\labl\n" ;
+	cout << "Good luck!" ;
+	return 0 ;
+}
