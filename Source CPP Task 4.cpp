e0eefa54158fbde0c2d333c85fca543c14183fd6
Abdul Hadi <hadigakhar601@gmail.com>
2026-02-02 11:30:40 +0500

    Delete Source CPP Task 4.cpp

--- Source CPP Task 4.cpp
+++
@@ -1,9 +0,0 @@
-# include <iostream>
-using namespace std;
-int main ()
-{
-	int arr[]= {10, 20, 30, 40, 50} ;
-	int length= sizeof(arr)/sizeof(arr[0]);
-	cout << "Array length: " << length << endl;
-	return 0;
-}
